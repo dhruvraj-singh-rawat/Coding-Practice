ac6befb487928adcceb428389e395a5aa4af2e7c
Dhruvraj Rawat <dhruvraj-singh-rawat@users.noreply.github.com>
2018-09-19 01:32:53 +0530

    Added: CCI 1.3

--- Cracking the Coding Interview/Arrays and Strings/1.3 URLify.cpp
+++ Cracking the Coding Interview/Arrays and Strings/1.3 URLify.cpp
@@ -1,11 +1,33 @@
 #include<iostream>
+#include<stack>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main(){
-    char a[3];
-    a[0]='a';
-    a[1]=' ';
-    a[2]='b';
+    char str[13];
+    cout<<"Enter the String: ";
+    cin.getline(str,13);
+    
+    stack<char> sk;
+    int check=0;
+    for(int i=strlen(str)-1;i>=0;i--){
+        if(str[i]==' ' && check !=0){
+            sk.push('0');
+            sk.push('2');
+            sk.push('%');
+            //cout<<"Here";
+        }
+        else if(str[i]!=' '){
+            sk.push(str[i]);
+            check=1;
+            //cout<<i<<endl;
+        }
 
-    cout<<int(a[1]);
+    }
+    //cout<<sk.size()<<endl;
+    while(!sk.empty()){
+        cout<<sk.top();
+        sk.pop();
+    }
+
 }
