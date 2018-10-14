6f9719947a0f12070e81882e3c80dfc6d3182078
Dhruvraj Rawat <dhruvraj-singh-rawat@users.noreply.github.com>
2018-10-15 02:56:22 +0530

    Fixed: Bug in CCI 2.1 #2

--- Cracking the Coding Interview/2. Linked List/2.1 Remove Dups #2.cpp
+++ Cracking the Coding Interview/2. Linked List/2.1 Remove Dups #2.cpp
@@ -18,6 +18,7 @@ int main(){
         node *temp_node=new node;
         cin>>temp;
         temp_node->data=temp;
+        temp_node->next=NULL;
 
         if(head==NULL){
             head=temp_node;
@@ -29,35 +30,38 @@ int main(){
         }
         
     }
-    cout<<"Printing the Inseted Numbers in Linked List: \n";
+    cout<<"\nPrinting the Inseted Numbers in Linked List: \n";
     node *head_1=head;
     while(head_1!=NULL){
         cout<<head_1->data<<" ";
         head_1=head_1->next;
     }
 
-    cout<<"\nNow removing the Duplicated: \n";
+    //cout<<"\nNow removing the Duplicated: \n";
     node *head_2=head;
 
     while(head_2!=NULL){
         node *head_3=head_2;
-        cout<<"Here 1";
+        //cout<<"Here 1: ";
         while(head_3!=NULL){
-            if(head_2->data==head_3->next->data && head_3->next != NULL){
+            if(head_3->next != NULL && head_2->data==head_3->next->data ){
                 head_3->next=head_3->next->next;
-                cout<<"Here 2";
+               // cout<<"Here 2: ";
             }
-            else{
+            else {
+                
+                //cout<<"|"<<head_3->data<<"|";
                 head_3=head_3->next;
-                cout<<head_3->data;
+                //cout<<"Here 4: ";
+                //cout<<head_3<<" ";
             }
-            //cout<<"Here 4";
+            
         }
-        cout<<"Here 5";
+        //cout<<"Here 5: ";
         head_2=head_2->next;
     }
 
-    cout<<"Printing the DeDuplicated Numbers in Linked List: \n";
+    cout<<"\nPrinting the DeDuplicated Numbers in Linked List: \n";
     node *head_4=head;
     while(head_4!=NULL){
         cout<<head_4->data<<" ";
