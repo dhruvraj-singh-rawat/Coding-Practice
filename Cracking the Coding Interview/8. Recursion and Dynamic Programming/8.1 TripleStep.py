98cd04dbbc448855f2c5e402355d3e6ed12c00b7
Dhruvraj Rawat <dhruvraj.rawat@gmail.com>
2019-08-31 13:05:48 +0530

    Added: 8.1 Triple Step Solution

---
+++ Cracking the Coding Interview/8. Recursion and Dynamic Programming/8.1 TripleStep.py
@@ -0,0 +1,16 @@
+def num_steps(step_arr,steps):
+	if(steps == 0):
+		return 1
+	if (steps <0):
+		return 0
+
+	if(step_arr[steps-1]==0):
+		step_arr[steps-1] = num_steps(step_arr,steps-1)+num_steps(step_arr,steps-2)+num_steps(step_arr,steps-3)
+
+	return step_arr[steps-1]
+
+steps = int(input('Enter the Stair Case length: '))
+
+step_arr = [0]*steps
+
+print('The Number of the Unique Steps possible are: ',num_steps(step_arr,steps))
