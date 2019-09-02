da6c36f8620071f903727144f892bbf342ca2747
Dhruvraj Rawat <dhruvraj.rawat@gmail.com>
2019-09-02 13:41:08 +0530

    Added: Problem Statement

--- Cracking the Coding Interview/8. Recursion and Dynamic Programming/8.2 FindingMath.py
+++ Cracking the Coding Interview/8. Recursion and Dynamic Programming/8.2 FindingMath.py
@@ -6,6 +6,14 @@ Created on Mon Sep  2 10:12:20 2019
 @author: dhruvrajrawat
 """
 
+# [8.2] Robot in a Grid: Imagine a robot sitting on the upper
+# left corner of grid with r rows and c columns. The robot can
+# only move in two directions, right and down, but certain
+# cells are "off limits" such that the robot cannot step on
+# them. Design an algorithm to find a path for the robot from
+# the top left to the bottom right.
+
+
 maze = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,'b',0,0]]
 
 
