f32450deb31bc59893f6de78d23e0df8103e0a11
Dhruvraj Rawat <dhruvraj.rawat@gmail.com>
2019-12-26 10:39:10 +0530

    Removed: Debugging Print commands

--- Cracking the Coding Interview/8. Recursion and Dynamic Programming/8.12 n_queen.py
+++ Cracking the Coding Interview/8. Recursion and Dynamic Programming/8.12 n_queen.py
@@ -1,16 +1,10 @@
 def diag(visited,pos,loc):
     for i in range(pos):
-        #print('The Previous Row: ',i,' the column pos: ',visited[i])
-        #print('The current Row: ',pos,' the column pos: ',loc)
-        
+      
         
         if (abs(i-(pos)) == abs(visited[i]-loc)):
-            #print('Diagonal')
-            #print('')
             return True
     else:
-        #print('NOT-Diagonal')
-        #print('')
         return False
 
 def recursive(visited,pos):
@@ -20,11 +14,11 @@ def recursive(visited,pos):
         return
     
     for i in range(len(visited)):
-        #print(visited)
+      
         if i in visited:
             continue
         elif ( diag(visited,pos,i)):
-            #print('here')
+         
             continue
         else:
             visited[pos]=i
